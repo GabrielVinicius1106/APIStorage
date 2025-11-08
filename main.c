b81acf53982bbfb924bbc5a714fb66e8e090b68a
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2025-11-08 10:01:44 -0300

    iniciar validação de entrada

--- main.c
+++ main.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "src/theme/theme.h"
 #include "src/structs/structs.h"
+#include "src/validateInput.h"
 
 int main(){ 
 
+    char *input;
+
+    system("clear");
+
+    printf(INPUT " >> ");
+    scanf("%s", input);
+
+    int option = validateInput(input); 
+
+    switch(option){
+    case 1:
+        // ADD
+        printf("Adicionar Chave! \n");
+        break;
+
+    case 2:
+        // DEL
+        printf("Deletar Chave! \n");
+        break;
+
+    case 3:
+        // EDIT
+        printf("Editar Chave! \n");
+        break;
+        
+    case 4:
+        // LIST
+        printf("Listar Chaves! \n");
+        break;
+    
+    case 5:
+        // HELP
+        printf("Ajuda! \n");
+        break;
+    
+    default:
+        break;
+    }
     
 
+    printf(RESET "\n");
 
 
     return 0;
